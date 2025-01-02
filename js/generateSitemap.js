95308dcdf5058fd5b166f48d2e493097d1cb586b
sunheyi <1061867552@qq.com>
2025-01-02 17:43:07 +0800

    change2 generateSitemap.js

--- js/generateSitemap.js
+++ js/generateSitemap.js
@@ -4,7 +4,7 @@ const { SitemapStream } = require('sitemap');
 // Creates a sitemap object given the input configuration with URLs
 const sitemap = new SitemapStream({ hostname: 'https://www.2048qp.com' });
 
-const writeStream = createWriteStream('./sitemap.xml');
+const writeStream = createWriteStream('././sitemap.xml');
 sitemap.pipe(writeStream);
 
 sitemap.write({ url: '/', changefreq: 'daily', priority: 0.3 });
